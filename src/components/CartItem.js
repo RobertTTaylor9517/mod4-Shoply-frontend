1c26902e93b21364840e56377368a60c795db9d1
RobertTTaylor9517 <robt15@hotmail.com>
2020-02-14 09:25:17 -0500

    friday

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,7 +6,7 @@ const CartItem =(props)=>{
     return(
         <Segment>
             <Header>{props.product.name}</Header>
-            <Image src='https://media.gettyimages.com/photos/different-types-of-food-on-rustic-wooden-table-picture-id861188910?s=2048x2048' size='small'/>
+            <Image src={props.product.img} size='small'/>
             <div>Price: ${props.product.price}</div>
             <Button onClick={()=>props.removeFromCart(props.product)}>Remove From Cart</Button>
         </Segment>
